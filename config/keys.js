5beec7ad78f6da4647fb2c74e687017cdd505865
SergeyZimakov <zimakovs93@gmail.com>
2021-05-16 17:25:31 +0300

    a

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,6 @@
-// if(process.env.NODE_ENV === 'production') {
-//     module.exports = require('./keys.prod');
-// } else {
-//     module.exports = require('./keys.dev');
-// }
+if(process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys.prod');
+} else {
+    module.exports = require('./keys.dev');
+}
 
-module.exports = {
-    mongoURI: 'mongodb+srv://sergey:9oZZ5eTOLfqKjpE0@cluster0.vnfgt.mongodb.net/CRMMEANDB?retryWrites=true&w=majority',
-    jwt: 'dev-jwt'
-};
